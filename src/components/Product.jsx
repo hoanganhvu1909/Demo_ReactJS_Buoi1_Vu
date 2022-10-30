b6eaabb1edf3faa88c6df0e22be50284404115d1
hoanganhvu1909 <cqt12ianhvu@gmail.com>
2022-10-30 13:24:43 +0700

    update

---
+++ src/components/Product.jsx
@@ -0,0 +1,19 @@
+// rfc: function component
+
+import React from 'react'
+
+export default function Product() {
+  return (
+    <div>
+      <div className='card'>
+        <img src="http://picsum.photo/id/200/200" alt="..." />
+        <div className="card-body">
+            <h4>Tên sản phẩm</h4>
+            <p>Giá
+
+            </p>
+        </div>
+      </div>
+    </div>
+  )
+}
