a1bfbb10eb1e82d0f67d542af48e56c39870923f
hoanganhvu1909 <cqt12ianhvu@gmail.com>
2022-11-10 17:32:22 +0700

    btxemchitiet

--- src/State/BaiTapXemChiTiet/Phone.jsx
+++ src/State/BaiTapXemChiTiet/Phone.jsx
@@ -1,11 +1,28 @@
-// rcc
-
 import React, { Component } from 'react'
 
 export default class Phone extends Component {
-  render() {
-    return (
-      <div>Phone</div>
-    )
-  }
+
+    render() {
+        const {phone,xemChiTiet} = this.props;
+        return (
+            <div className="card">
+                <img
+                    src={phone.hinhAnh}
+                    alt="..."
+                    height={450}
+                    style={{ objectFit: "cover" }}
+                />
+                <div className="card-body">
+                    <h3>{phone.tenSP}</h3>
+                    <p>{phone.giaBan}</p>
+                    <button className="btn btn-dark" onClick={() => {
+                        // xemChiTiet(phone);
+                        this.props.xemChiTiet(phone)
+                    }}>Xem chi tiết</button>
+                </div>
+            </div>
+        )
+    }
 }
+
+
